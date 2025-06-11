13c5876fda99e71250900439127026a070a3a51d
Bohdana Prokopchuk <b.prokopchuk@ukma.edu.ua>
2025-06-12 02:51:12 +0300

    Homepage UI changed

--- reelchoice_app/views.py
+++ reelchoice_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+import random
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from .services import search_movies_by_title, get_user_ratings_data, get_movies_by_genre, get_movie_detail_info, \
@@ -8,43 +9,42 @@ from .models import Genre, Movie
 
 @login_required
 def home(request):
+    user = request.user
+
+    # Viewers' Choice
+    top_movies = Movie.objects.order_by('-vote_average')[:50]
+    viewers_choice = random.sample(list(top_movies.values('title', 'poster_path', 'vote_average')), 5)
+
+    # Recommended for you
+    all_ids = list(Movie.objects.values_list('id', flat=True))
+    recommended_ids = random.sample(all_ids, min(5, len(all_ids)))
+    recommended = Movie.objects.filter(id__in=recommended_ids).values('title', 'poster_path', 'vote_average')
+
+    # True Story
+    true_story_qs = Movie.objects.filter(overview__icontains='true story')
+    true_story = random.sample(list(true_story_qs.values('title', 'poster_path', 'vote_average')), min(5, true_story_qs.count()))
+
+    # Horror
+    horror_qs = Movie.objects.filter(genres__name__iexact='Horror')
+    horror = random.sample(list(horror_qs.values('title', 'poster_path', 'vote_average')), min(5, horror_qs.count()))
+
+    # Rate More Movies
+    if user.is_authenticated:
+        rated_ids = user.ratings.values_list('movie_id', flat=True)
+        unrated_qs = Movie.objects.exclude(id__in=rated_ids)
+    else:
+        unrated_qs = Movie.objects.all()
+
+    unrated = random.sample(list(unrated_qs.values('title', 'poster_path', 'vote_average')), min(5, unrated_qs.count()))
+
     sections = [
-        {
-            "title": "Viewers' Choice",
-            "movies": [
-                {"title": "Inception", "poster_path": "/inception.jpg", "vote_average": 8.8},
-                {"title": "The Dark Knight", "poster_path": "/darkknight.jpg", "vote_average": 9.0},
-            ],
-        },
-        {
-            "title": "Recommended for you",
-            "movies": [
-                {"title": "Interstellar", "poster_path": "/interstellar.jpg", "vote_average": 8.6},
-                {"title": "Arrival", "poster_path": "/arrival.jpg", "vote_average": 7.9},
-            ],
-        },
-        {
-            "title": "Based on a true story",
-            "movies": [
-                {"title": "Catch Me If You Can", "poster_path": "/catchmeifyoucan.jpg", "vote_average": 8.1},
-                {"title": "The Imitation Game", "poster_path": "/imitationgame.jpg", "vote_average": 8.0},
-            ],
-        },
-        {
-            "title": "Top Horror Movies",
-            "movies": [
-                {"title": "The Conjuring", "poster_path": "/conjuring.jpg", "vote_average": 7.5},
-                {"title": "It", "poster_path": "/it.jpg", "vote_average": 7.3},
-            ],
-        },
-        {
-            "title": "Rate More Movies",
-            "movies": [
-                {"title": "Joker", "poster_path": "/joker.jpg", "vote_average": 8.5},
-                {"title": "Parasite", "poster_path": "/parasite.jpg", "vote_average": 8.6},
-            ],
-        },
+        {"title": "Viewers' Choice", "movies": viewers_choice},
+        {"title": "Recommended for you", "movies": recommended},
+        {"title": "Based on a true story", "movies": true_story},
+        {"title": "Top Horror Movies", "movies": horror},
+        {"title": "Rate More Movies", "movies": unrated},
     ]
+
     return render(request, "home.html", {"sections": sections})
 
 
