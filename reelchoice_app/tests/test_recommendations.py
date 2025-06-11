b215d4491dbaaeb1faa4f2e810c9c42a6d89d865
Denys Solomennyi <denisonovich@gmail.com>
2025-06-11 14:54:31 +0300

    Added tests for recommendations

---
+++ reelchoice_app/tests/test_recommendations.py
@@ -0,0 +1,51 @@
+import os
+
+from django.conf import settings
+from django.test import TestCase
+
+from recommender.recommender import ItemBasedCF
+
+
+class TestRecommendations(TestCase):
+    def setUp(self):
+        self.model = ItemBasedCF()
+        model_path = os.path.join(settings.BASE_DIR, 'recommender', 'trained_model.pkl')
+        self.model.load_model(model_path)
+
+        self.sample_user_ratings = {
+            27205: 5.0,  # Inception
+            157336: 4.5,  # Interstellar
+            155: 3.5,  # The Dark Knight
+            19995: 4.5,  # Avatar
+            24428: 4.0  # The Avengers
+        }
+
+    def test_model_loaded_successfully(self):
+        self.assertIsNotNone(self.model.item_similarities)
+        self.assertIsNotNone(self.model.item_means)
+        self.assertIsNotNone(self.model.all_items)
+        self.assertGreater(len(self.model.all_items), 0)
+
+    def test_predict_rating(self):
+        test_movie_id = 550  # Fight Club
+
+        if test_movie_id in self.model.item_similarities and test_movie_id in self.model.item_means:
+            predicted_rating = self.model.predict_rating(self.sample_user_ratings, test_movie_id)
+            self.assertIsNotNone(predicted_rating)
+            self.assertGreaterEqual(predicted_rating, 1.0)
+            self.assertLessEqual(predicted_rating, 5.0)
+        else:
+            self.skipTest(f"Movie ID {test_movie_id} not found in the similarities to user ratings")
+
+    def test_recommend_items(self):
+        recommendations = self.model.recommend_items(self.sample_user_ratings, n_recommendations=20)
+
+        self.assertIsNotNone(recommendations)
+
+        for movie_id, rating in recommendations:
+            self.assertNotIn(movie_id, self.sample_user_ratings)
+            self.assertGreaterEqual(rating, 1.0)
+            self.assertLessEqual(rating, 5.0)
+
+        ratings = [rating for _, rating in recommendations]
+        self.assertEqual(ratings, sorted(ratings, reverse=True))
